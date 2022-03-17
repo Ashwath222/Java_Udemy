ffd11d0cad461e7a78c05dfefb0047965a56417a
Ashwath222 <ashwathreddyd222@gmail.com>
2022-03-17 15:45:56 +0530

    Section : 11 Packages.

---
+++ series/Series.java
@@ -0,0 +1,20 @@
+package com.example.series;
+
+public class Series {
+
+    public static int nSum(int n)  {
+        return n * (n-1) / 2;
+    }
+
+    public static int factorial (int n) {
+        if(n==0 || n==1)
+            return 1;
+        return n * factorial(n-1);
+    }
+
+    public static int fibonacci(int n) {
+        if ( n ==0 || n == 1)
+            return n;
+        return fibonacci(n-1) + fibonacci(n-2);
+    }
+}
